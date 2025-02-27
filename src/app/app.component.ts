4a420ad3d7acf9250cac5aaa4512451a65b84b1c
Harshad080 <harshadpal.0987@gmail.com>
2025-02-27 17:29:00 +0530

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'outFirstApp';
+  title = 'I Love Angular from TS file !!!';
+
+  skills : Array<string> = ["JS", "TS", "Angular", "Node"]
 }
